dbd14ae4b5a82b8f3777f45f2b9f5f100af139a1
QuentinCouavoux <121060440+QuentinCouavoux@users.noreply.github.com>
2023-03-14 18:08:13 +0100

    la chose

---
+++ NTS/Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float horizontal;
+    private bool inputJump;
+    private bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inputJump && isGrounded)
+        {
+            inputJump = Input.GetKeyDown(KeyCode.Space);
+        }
+
+        horizontal = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if (inputJump && isGrounded)
+        {
+            rb.AddForce(Vector3.up*5,ForceMode.Impulse);
+            inputJump = false;
+            isGrounded = false;
+        }
+
+        if (horizontal != 0)
+        {
+            transform.Translate(Vector3.right*horizontal*Time.deltaTime*5);
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        isGrounded = false;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
