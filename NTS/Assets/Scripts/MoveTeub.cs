1cf01b738a2bb510a768d0719a5542bd8c4672ca
Léo Menaldo <leo.menaldo@epita.fr>
2023-03-15 02:36:34 +0100

    [Map Generation] : Beaautiful

--- NTS/Assets/Scripts/MoveTeub.cs
+++ NTS/Assets/Scripts/MoveTeub.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine;
-using UnityEngine.Experimental.GlobalIllumination;
-
 public class MoveTeub : MonoBehaviour
 {
     private GameObject player;
--- NTS/Assets/Scripts/TerrainGeneration.cs
+++ NTS/Assets/Scripts/TerrainGeneration.cs
@@ -1,18 +1,37 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Random = System.Random;
-
 public class TerrainGeneration : MonoBehaviour
 {
     public GameObject dirtFloor;
+    public GameObject dirt;
+    public GameObject startButton;
+    public GameObject mainMenuCanvas;
+    public GameObject mainMenuScreen;
+    public GameObject leJoueur;
+    public GameObject tree;
+
     void Start()
     {
-        Random rd = new Random();
-        bool[,] Map = new bool[200, 10];
+        mainMenuCanvas.SetActive(true);
+        mainMenuCanvas.SetActive(true);
+        startButton.SetActive(true);
+        mainMenuScreen.SetActive(true);
+        dirt.SetActive(true);
+        leJoueur.SetActive(false);
+        dirtFloor.SetActive(true);
+    }
+
+    public void Creation()
+    {
+        mainMenuCanvas.SetActive(true);
+        startButton.SetActive(false);
+        leJoueur.SetActive(true);
+        mainMenuScreen.SetActive(false);
+        
+        System.Random rd = new System.Random();
+        bool[,] map = new bool[200, 10];
         int randStart = rd.Next(0, 10);
-        Map[0, randStart] = true;
+        map[0, randStart] = true;
         for (int i = 1; i < 200; i++)
         {
             int randTemp = rd.Next(0, 10);
@@ -21,20 +40,45 @@ public class TerrainGeneration : MonoBehaviour
                 randTemp = rd.Next(0, 10);
             }
 
-            Map[i, randTemp] = true;
+            map[i, randTemp] = true;
             randStart = randTemp;
         }
 
+        int treeDelay = rd.Next(5, 10);
+        
         for (int i = 0; i < 200; i++)
         {
+            treeDelay -= 1;
+            bool floorPlaced = true;
             for (int j = 0; j < 10; j++)
             {
-                if (Map[i, j])
+                if (floorPlaced)
                 {
                     Vector3 temp = new Vector3(i, j, 0);
+                    Instantiate(dirt, temp, Quaternion.identity);
+                }
+                
+                if (map[i, j])
+                {
+                    Vector3 temp = new Vector3(i, j, 0);
+                    floorPlaced = false;
                     Instantiate(dirtFloor, temp, Quaternion.identity);
+                    
+                    if (treeDelay < 0)
+                    {
+                        Vector3 otherTemp = new Vector3(i, j, 1);
+                        Instantiate(tree, otherTemp, Quaternion.identity);
+                        treeDelay = rd.Next(5, 20);
+                    }
                 }
             }
         }
+
+        for (int j = 0; j < 15; j++)
+        {
+            Vector3 temp = new Vector3(-1, j, 0);
+            Instantiate(dirt, temp, Quaternion.identity);
+        }
+        Instantiate(dirtFloor, new Vector3(-1, 15, 0), Quaternion.identity);
     }
 }
